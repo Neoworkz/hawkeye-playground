ed652ca6019e3876db243f9a08264f64dc657907
amguist <rhon.guist@gmail.com>
2024-05-08 14:00:10 -0700

    Adding pull request template

---
+++ services/hawkeye-playground-api/src/main/java/org/neoworkz/playground/services/api/PlaygroundRestServiceRunner.java
@@ -0,0 +1,8 @@
+package org.neoworkz.playground.services.api;
+
+public class PlaygroundRestServiceRunner {
+
+    public static void main(final String[] args) throws Exception {
+        System.out.println("Starting up Playground Rest Service .....");
+    }
+}
